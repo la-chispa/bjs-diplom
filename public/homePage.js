12ded31701596ffb70e29cb171a09eb05f5925af
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-07-15 22:09:20 +0600

    Done. Maybe

--- public/homePage.js
+++ public/homePage.js
@@ -1,7 +1,99 @@
 const userLogout = new LogoutButton();
-userLogout.action = logout => console.log(logout);
-userLogout.action = (logout) => ApiConnector.logout(response => {
-	if (response) {
-		return location.reload();
+userLogout.action = () => {
+	ApiConnector.logout((response) => {
+		if (response.success) {
+			location.reload();
+		}
+	});
+}
+
+ApiConnector.current((response) => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
 	}
 })
+
+const userRatesBoard = new RatesBoard();
+getRatesBoard = () => {
+	ApiConnector.getStocks((response) => {
+		if (response.success) {
+			userRatesBoard.clearTable();
+			userRatesBoard.fillTable(response.data);
+		}
+	})
+}
+getRatesBoard();
+setInterval(getRatesBoard, 60000);
+
+const userMoneyManager = new MoneyManager();
+userMoneyManager.addMoneyCallback = (data) => ApiConnector.addMoney(data, response => {
+	if (response.success) {
+		userMoneyManager.setMessage(false, `Вы пополнили счёт на ${data.amount} ${data.currency}`);
+		ProfileWidget.showProfile(response.data);
+	} else { 
+		userMoneyManager.setMessage(true, response.data);
+	}
+});
+
+userMoneyManager.conversionMoneyCallback = (data) => ApiConnector.convertMoney(data, response => {
+	if (response.success) {
+		userMoneyManager.setMessage(false, `Вы конвертировали ${data.fromAmount} ${data.fromCurrency} в ${data.targetCurrency}`);
+		ProfileWidget.showProfile(response.data);
+	} else { 
+		userMoneyManager.setMessage(true, response.data);
+	}
+})
+
+userMoneyManager.sendMoneyCallback = (data) => ApiConnector.transferMoney(data, response => {
+	if (response.success) {
+		userMoneyManager.setMessage(false, `Вы перевели ${data.amount} ${data.currency}`);
+		ProfileWidget.showProfile(response.data);
+	} else { 
+		userMoneyManager.setMessage(true, response.data);
+	}
+})
+
+const userFavoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites((response) => {
+	if (response.success) {
+		userFavoritesWidget.clearTable();
+		userFavoritesWidget.fillTable(response.data);
+		userMoneyManager.updateUsersList(response.data);
+	}
+})
+
+userFavoritesWidget.addUserCallback = (data) => ApiConnector.addUserToFavorites(data, response => {
+	if (response.success) {
+		userFavoritesWidget.setMessage(false, `Вы добавили пользователя ${data.name} в адресную книгу`)
+		userFavoritesWidget.clearTable();
+		userFavoritesWidget.fillTable(response.data);
+		userMoneyManager.updateUsersList(response.data);
+	} else {
+		userFavoritesWidget.setMessage(true, response.data);
+	}
+})
+
+userFavoritesWidget.removeUserCallback = (data) => ApiConnector.removeUserFromFavorites(data, response => {
+	if (response.success) {
+		userFavoritesWidget.setMessage(false, `Вы удалили пользователя из адресной книги`)
+		userFavoritesWidget.clearTable();
+		userFavoritesWidget.fillTable(response.data);
+		userMoneyManager.updateUsersList(response.data);
+	} else {
+		userFavoritesWidget.setMessage(true, response.data);
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const userForm = new UserForm();
-userForm.loginFormCallback = data => console.log(data);
+
 userForm.loginFormCallback = (data) => ApiConnector.login(data, response => {
-	if (!response.success) {
-		return setLoginErrorMessage(message);
-	} else {
-		return location.reload();
-	}
+	if (response.success) {
+    	location.reload();
+  	} else {
+    	userForm.setLoginErrorMessage(response.data);
+  	}
 });
 
 userForm.registerFormCallback = (data) =>  ApiConnector.register(data, response => {
-	if (!response.success) {
-		return registerFormCallback(message);
+	if (response.success) {
+		location.reload();
 	} else {
-		return location.reload();
+		userForm.setRegisterErrorMessage(response.data);
 	}
 })
